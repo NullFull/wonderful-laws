9b71c65046323995937ccde5678479f5a1edca90
constmoon <jiwoo.link@gmail.com>
2021-02-23 03:46:35 +0900

    서명 폼 validation 추가

--- src/components/Form.js
+++ src/components/Form.js
@@ -19,11 +19,12 @@ const Label = styled.label({
     fontSize: '16px',
 })
 
-const Input = styled.input({
+const Input = styled.input(props =>({
     ...common,
     display: 'block',
     width: '100%',
-})
+    border: props.error ? '1px solid #ff0000' : common.border
+}))
 
 const Textarea = styled.textarea({
     ...common,
@@ -31,21 +32,66 @@ const Textarea = styled.textarea({
     width: '100%',
 })
 
+const CheckBoxLabel = styled.label({
+    position: 'relative',
+    display: 'inline-block',
+    maxWidth: '100%',
+    fontSize: '16px',
+    cursor: 'pointer'
+})
+
+const CheckBoxLabelText = styled.span({
+    paddingLeft: '30px'
+})
+
+const CheckBoxInput = styled.input(props=>({
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: '1px',
+    height: '1px',
+    margin: 0,
+    cursor: 'pointer',
+    '&::before': {
+        content: '""',
+        position: 'absolute',
+        top: '0',
+        left: '0',
+        width: '15px',
+        height: '15px',
+        border: props.error ? '1px solid #ff0000' : common.border,
+        backgroundColor: '#ffffff'
+    },
+    '&::after': {
+        content: '"✓"',
+        position: 'absolute',
+        left: '4px',
+        top: '-1px',
+        fontSize: '19px',
+        opacity: 0
+    },
+    '&:checked::after': {
+        opacity: 1
+    }
+}))
+
 const CheckBox = forwardRef((props, ref) => (
-    <input type="checkbox" ref={ref} {...props} />
+    <CheckBoxInput type="checkbox" ref={ref} {...props} />
 ))
 
-const Hint = styled.p({
+const Hint = styled.p(props => ({
     fontSize: '12px',
     padding: '4px 12px',
     margin: 0,
-})
+    color: props.error && 'red'
+}))
 
 const InputGroup = styled.div({
-    padding: '8px 0 0',
+    padding: '10px 0 0',
 })
 
 const CheckGroup = styled(InputGroup)({
+    padding: '15px 0 0',
     [Hint]: {
         paddingLeft: '32px',
     }
@@ -55,7 +101,7 @@ const CheckGroup = styled(InputGroup)({
 const Form = () => {
     const [loading, setLoading] = useState(false)
     const [submitted, setSubmitted] = useState(false)
-    const { register, handleSubmit } = useForm()
+    const { register, errors, handleSubmit } = useForm()
 
     const submit = async data => {
         setLoading(true)
@@ -93,21 +139,27 @@ const Form = () => {
 
     return (
         <div css={{ textAlign: 'left' }}>
-            <form onSubmit={handleSubmit(submit)}>
+            <form onSubmit={handleSubmit(submit)} >
                 <InputGroup>
                     <Label>이름</Label>
-                    <Input ref={register} name="name" />
-                    <Hint>실명을 입력해주셔야 유효한 서명이 됩니다</Hint>
+                    <Input ref={register({ required: true })} name="name" error={errors.name} />
+                    {errors.name
+                        ? <Hint error={errors.name}>이름을 적어주세요</Hint>
+                        : <Hint>실명을 입력해주셔야 유효한 서명이 됩니다</Hint>}
                 </InputGroup>
                 <InputGroup>
                     <Label>연락처</Label>
-                    <Input ref={register} name="contact" />
-                    <Hint>휴대전화번호 혹은 이메일</Hint>
+                    <Input ref={register({ required: true })} name="contact" error={errors.contact} />
+                    {errors.contact
+                        ? <Hint error={errors.contact}>연락처를 적어주세요</Hint>
+                        : <Hint>휴대전화번호 혹은 이메일</Hint>}
                 </InputGroup>
                 <InputGroup>
                     <Label>주소</Label>
-                    <Input ref={register} name="address" />
-                    <Hint>읍/면/동 까지만</Hint>
+                    <Input ref={register({ required: true })} name="address" error={errors.address} />
+                    {errors.address
+                        ? <Hint error={errors.address}>주소를 적어주세요</Hint>
+                        : <Hint>읍/면/동 까지만</Hint>}
                 </InputGroup>
                 <InputGroup>
                     <Label>한마디</Label>
@@ -118,18 +170,19 @@ const Form = () => {
                     />
                 </InputGroup>
                 <CheckGroup>
-                    <Label>
+                    <CheckBoxLabel>
                         <CheckBox ref={register} name="private" />
-                        비공개
-                    </Label>
+                        <CheckBoxLabelText>비공개</CheckBoxLabelText>
+                    </CheckBoxLabel>
                     <Hint>체크하시면 목록에 표시되지 않으며 담당자만 볼 수 있습니다.</Hint>
                 </CheckGroup>
                 <CheckGroup>
-                    <Label>
-                        <CheckBox ref={register} name="agreed" />
-                        개인정보수집동의 (필수)
-                    </Label>
+                    <CheckBoxLabel>
+                        <CheckBox ref={register({ required: true })} name="agreed" error={errors.agreed}/>
+                        <CheckBoxLabelText>개인정보수집동의 (필수)</CheckBoxLabelText>
+                    </CheckBoxLabel>
                     <Hint>제출하신 개인정보는 국회 제출 외의 용도로 사용되지 않으며 귀하의 동의 없이 제3자에게 제공하지 않습니다.</Hint>
+                    {errors.agreed && <Hint error={errors.agreed}>개인정보수집에 동의하셔야 서명이 가능합니다</Hint>}
                 </CheckGroup>
                 <InputGroup>
                     <Button disabled={loading} style={{ width: '100%' }}>서명하기</Button>
