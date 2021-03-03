c250632966d4cfc6787c5f04a3f62a9a9308eafd
lexifdev <sl@lxf.kr>
2021-03-04 00:19:22 +0900

    기본 OG 정보 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,29 @@
-import { Global } from '@emotion/react'
 import React from 'react'
-
+import { Global } from '@emotion/react'
+import { DefaultSeo } from 'next-seo'
+import ogImage from 'assets/og-game.png'
 
 const App = ({ Component, pageProps }) => {
     return (
         <>
+            <DefaultSeo
+                title="이상한 나라의 강간죄. 당신의 선택과 판결은?"
+                description="사건을 살펴보고 정의로운 판결을 내려보세요. "
+                canonical="https://wonderful-law.korea.wtf/"
+                openGraph={{
+                    type: 'website',
+                    url: 'https://wonderful-law.korea.wtf/',
+                    images: [{
+                        url: ogImage,
+                        alt: '이상한 나라의 성범죄 사건들. 당신의 판결은?'
+                    }]
+                }}
+                twitter={{
+                    handle: '@handle',
+                    site: '@site',
+                    cardType: 'summary_large_image',
+                }}
+            />
             <Global
                 styles={{
                     '*': {
